7b2980ff6c1a21d92cb752cd740f6b483c97c91e
unknown <washingtonmya@gmail.com>
2022-07-12 10:42:22 -0700

    update

---
+++ logic.js
@@ -0,0 +1,9 @@
+const timerConainerEl = document.getElementById("timer-container");
+const timerTextEl = document.getElementById("timer-text");
+const directionTextEl = document.getElementById("direction-text");
+const startQuizButton = document.getElementById("start-quiz-btn");
+const questionContainerEl = document.getElementById("quiz-container");
+const choiceContainerEl = document.getElementById("choice-container");
+const submitScoreFormEl = document.getElementById("submit-score-form");
+const submitScoreButtonEl = document.getElementById("submit-score-btn");
+
---
+++ questions.js
@@ -0,0 +1,17 @@
+var questions = [
+    {
+        questionsText: "Which symbol is used for the assignment operator?",
+        choices: ["while", "for", "try"],
+        answer: "try"
+    },
+    {
+        questionsText: "Which of these is a JavaScript data type?",
+        choices: ["Fraction", "Boolean", "Nothing"],
+        answer: "Boolean"
+    },
+    {
+        questionsText: "Which of these is an escape character in JavaScript",
+        choices: ["*", "&", "\b"],
+        answer: "\b"
+    },
+]
